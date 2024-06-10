f4f3da505dbff9dc20117375aded4ee1b3aeeef1
raeis-i <raeisi.cloud@gmail.com>
2024-06-10 17:07:32 +0330

    update vars.rb

--- vars.rb
+++ vars.rb
@@ -1,8 +1,8 @@
 #You can use image name or download it and 
-#IMAGE_Ubuntu = "ubuntu/mantic64"
+IMAGE_Ubuntu = "ubuntu/mantic64"
 
 # If you have already download any boxes you can use same as below.
-IMAGE_Ubuntu = "../mantic-server-cloudimg-amd64-vagrant.box"
+#IMAGE_Ubuntu = "../mantic-server-cloudimg-amd64-vagrant.box"
 
 #Number of total nodes
 NUM_NODES = 3
